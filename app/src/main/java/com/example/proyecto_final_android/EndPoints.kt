c7e2d4346fa785549612ca062a4fa893b426bfa3
zareangel <miguelaz1@outlook.com>
2025-02-24 21:55:41 -0500

    Corregi el listado de venta

--- app/src/main/java/com/example/proyecto_final_android/EndPoints.kt
+++ app/src/main/java/com/example/proyecto_final_android/EndPoints.kt
@@ -1,13 +1,16 @@
 package com.example.proyecto_final_android
 
 object EndPoints {
-    private const val URL_LOGIN = "http://172.19.96.1:8080/api/v1"
-    private const val URL_VENTA = "http://172.19.96.1:8080/api/v3"
-    private const val URL_ROOT = "http://172.19.96.1:8080/api/v2"
+    private const val URL_LOGIN = "http://192.168.18.3:8080/api/v1"
+    private const val URL_VENTA = "http://192.168.18.3:8080/api/v3"
+    private const val URL_ROOT = "http://192.168.18.3:8080/api/v2"
 
     const val GET_ALL_PRODUCTS = "$URL_ROOT/allProducts"
     const val GET_CODE_PRODUCTS = "$URL_ROOT/code"
     const val GET_USER_LOGIN = "$URL_LOGIN/login"
 
     const val GET_ALL_SALES = "$URL_VENTA/allSales"
+    const val GET_NUMBER_SALES = "$URL_VENTA"
+
+
 }
